3715c9eae36b359a1e0944258c66a3c6bb7139b9
Chingis Choybsonov <avatarius93@gmail.com>
2024-02-11 18:25:33 +0300

    первая версия

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+`use strict`;
+
+const slides = document.querySelectorAll('.slider__list-item');
+let currentSlide = 0;
+
+const dotContainer = document.querySelector('.dot__container');
+const dotArray = Array.from(document.querySelectorAll('.dot'));
+
+const previousButton = document.querySelector('.slider-button_previous');
+const nextButton = document.querySelector('.slider-button_next');
+
+
+
+nextButton.addEventListener('click', next);
+previousButton.addEventListener('click', previous);
+dotContainer.addEventListener('click', function(evt) {
+  // dotContainer
+  const index = dotArray.indexOf(evt.target);
+  if (index !== -1) {
+    showSlide(index);
+    activateDot(index);
+  }
+})
+
+
+function showSlide(index) {
+  console.log(index);
+  slides.forEach( (item) => {
+    const imgWidth = item.clientWidth;
+    item.style.transform = `translateX(${-imgWidth * index}px)`;
+  } )
+}
+
+function activateDot(index) {
+  dotArray.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add('dot_active');
+    } else {
+      item.classList.remove('dot_active');
+    }
+  });
+}
+
+function next() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+  activateDot(currentSlide);
+
+}
+
+function previous() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+  activateDot(currentSlide);
+}
+
+
