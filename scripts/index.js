3c04494bb8bcdbd73fb4aec02f0792645cfaeb09
Chingis Choybsonov <avatarius93@gmail.com>
2024-02-11 19:40:11 +0300

    подключен слушатель на клавиатуру

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,42 @@
 `use strict`;
 
-const slides = document.querySelectorAll('.slider__list-item');
+const slides = document.querySelectorAll(".slider__list-item");
 let currentSlide = 0;
 
-const dotContainer = document.querySelector('.dot__container');
-const dotArray = Array.from(document.querySelectorAll('.dot'));
+const dotContainer = document.querySelector(".dot__container");
+const dotArray = Array.from(document.querySelectorAll(".dot"));
 
-const previousButton = document.querySelector('.slider-button_previous');
-const nextButton = document.querySelector('.slider-button_next');
+const previousButton = document.querySelector(".slider-button_previous");
+const nextButton = document.querySelector(".slider-button_next");
 
-
-
-nextButton.addEventListener('click', next);
-previousButton.addEventListener('click', previous);
-dotContainer.addEventListener('click', function(evt) {
-  // dotContainer
+nextButton.addEventListener("click", next);
+previousButton.addEventListener("click", previous);
+dotContainer.addEventListener("click", function (evt) {
   const index = dotArray.indexOf(evt.target);
   if (index !== -1) {
     showSlide(index);
     activateDot(index);
     currentSlide = index;
   }
-})
-
+});
+document.addEventListener('keydown', function(evt) {
+  if (evt.key === 'ArrowLeft') previous();
+  if (evt.key === 'ArrowRight') next();
+});
 
 function showSlide(index) {
-  slides.forEach( (item, i) => {
-    console.log(currentSlide);
+  slides.forEach((item, i) => {
     const imgWidth = item.clientWidth;
     item.style.transform = `translateX(${-imgWidth * index}px)`;
-  } )
+  });
 }
 
 function activateDot(index) {
   dotArray.forEach((item, i) => {
     if (i === index) {
-      item.classList.add('dot_active');
+      item.classList.add("dot_active");
     } else {
-      item.classList.remove('dot_active');
+      item.classList.remove("dot_active");
     }
   });
 }
@@ -46,7 +45,6 @@ function next() {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
   activateDot(currentSlide);
-
 }
 
 function previous() {
@@ -54,5 +52,3 @@ function previous() {
   showSlide(currentSlide);
   activateDot(currentSlide);
 }
-
-
